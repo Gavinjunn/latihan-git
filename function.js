52bd2d8f292a22046e99eb9f23cbcdbaf99a42e1
gavin lingga <gavinlingga@gavins-MacBook-Pro.local>
2018-05-31 21:35:50 +0700

    menambahkan fungsi

--- function.js
+++ function.js
@@ -11,3 +11,15 @@ var num1 = 5;
 var num2 = 6;
 var hasilPerkalian = calculateMultiply(num1, num2);
 console.log(hasilPerkalian);
+
+function prossesSentence(name, age, address, hobby){
+  return 'nama saya ' + name + ' umur saya ' + age + ' alamat saya di jalan ' + address + ' dan hobby saya ' + hobby;
+}
+
+var name = 'agus';
+var age = 30;
+var address = 'jalan malioboro, yogyakarta';
+var hobby = 'gaming';
+
+var fullSentence = prossesSentence(name, age, address, hobby);
+console.log(fullSentence);
