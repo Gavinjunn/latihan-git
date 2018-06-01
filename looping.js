104a6455c5ac0c429780436222d400e39055b662
gavin lingga <gavinlingga@gavins-MacBook-Pro.local>
2018-06-01 20:56:28 +0700

    membuat looping

---
+++ looping.js
@@ -0,0 +1,20 @@
+console.log('LOOPING PERTAMA'); 
+  var i=2;
+ while(i<=20){
+   console.log(i+" - I love coding")
+   i+=2
+ }
+ console.log('LOOPING KEDUA');
+ var j=20;
+ while(j>=2){
+   console.log(j+" - I will become fullstack developer")
+   j-=2  }
+
+  console.log("LOOPING PERTAMA");
+  for(var i=1; i<=20; i++){
+    console.log(i+" - I love coding")
+  }
+  console.log("LOOPING KEDUA");
+  for(var j=20; j>=1; j--){
+    console.log(j+" - I will become fullstack developer ")
+  }
